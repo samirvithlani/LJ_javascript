61d269d31f00845f27054eab1886159fb0207601
samirvithlani <samir.vithlani83955@gmail.com>
2024-12-10 17:25:55 +0530

    split

---
+++ string/string7.js
@@ -0,0 +1,13 @@
+var email = "amit@gmail.com";
+
+var flag = email.includes("@")
+//var flag = email.includes("@",7)
+console.log("flag  ",flag)
+
+var isStarts = email.startsWith("a")
+console.log("isStats",isStarts)
+var isEnds = email.endsWith(".com")
+console.log("isENds",isEnds)
+
+
+
---
+++ string/string8.js
@@ -0,0 +1,4 @@
+var address = "Surbhi Complex, 2nd and 3rd floor, Chimanlal Girdharlal Rd, Opposite Municipal Market, Vasant Vihar, Navrangpura, Ahmedabad, Gujarat 380009"
+
+var x = address.split(" ")
+console.log("x = ",x)
