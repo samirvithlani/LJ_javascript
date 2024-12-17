eadc4f21cfe266100ab19f0d21ddb6683e6a6027
samirvithlani <samir.vithlani83955@gmail.com>
2024-12-17 17:24:48 +0530

    functions

---
+++ functions/function1.js
@@ -0,0 +1,28 @@
+//without return type without param
+
+function test(){
+    
+    console.log("test function called....")
+}
+
+
+//without retutn yupe with arg...
+
+function sum(a,b){
+
+    console.log("value of a ",a)
+    console.log("value of a ",b)
+
+    let c  = a + b;
+    console.log(c)
+}
+
+test() //calling part...
+
+var no1 =100
+var no2 =20;
+//sum(100,200)
+//sum(no1,no2)
+//sum()
+//sum(10)
+sum(10,10,10)
