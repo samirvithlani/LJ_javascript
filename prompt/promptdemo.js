784afffa50de14880312e932e97e5776215933cc
samirvithlani <samir.vithlani83955@gmail.com>
2024-12-23 16:31:41 +0530

    add

--- prompt/promptdemo.js
+++ prompt/promptdemo.js
@@ -1,9 +1,35 @@
-//var x = prompt("please enter your age")
-var x = parseInt(prompt("please enter your age:\n ok"))
-console.log("value of x = ",x+100) //"12"
-console.log(typeof x)
+// //var x = prompt("please enter your age")
+// var x = parseInt(prompt("please enter your age:\n ok"))
+// console.log("value of x = ",x+100) //"12"
+// console.log(typeof x)
 
-//alert(x)
+// //alert(x)
 
+
+
+var no1 = parseInt(prompt("Enter No 1"))
+var no2 = parseInt(prompt("Enter No 2"))
+var choice = parseInt(prompt("Enter 1 for Add:\n Enter 2 Sub:\n Enter 3 For Mul"))
+
+
+function add(no1,no2){
+    return no1 + no2;
+}
+function sub(no1,no2){
+    return no1 - no2;
+}
+switch(choice){
+
+
+    case 1:
+        var ans = add(no1,no2)
+        alert(ans)
+        break;
+    case 2:
+        var ans = sub(no1,no2)
+        alert(ans)
+    break;
+
+}
 
 
