3c52a169787edaad02281d3302fa4e746fd3641a
samirvithlani <samir.vithlani83955@gmail.com>
2024-12-23 16:16:46 +0530

    prompt

---
+++ prompt/promptdemo.js
@@ -0,0 +1,9 @@
+//var x = prompt("please enter your age")
+var x = parseInt(prompt("please enter your age:\n ok"))
+console.log("value of x = ",x+100) //"12"
+console.log(typeof x)
+
+//alert(x)
+
+
+
