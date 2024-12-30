dd3b89478ebbc550471100ab13c6230bd03c8927
samirvithlani <samir.vithlani83955@gmail.com>
2024-12-30 16:49:20 +0530

    ladder terneray operator

---
+++ operator/ternerery2.js
@@ -0,0 +1,40 @@
+var no1 =100
+var no2 =200
+
+
+
+// if(no1>no2){
+
+//     console.log("no1 is greater")
+// }
+// else if(no1==no2){
+//     console.log("both are same")
+// }
+// else{
+//     console.log("no2 is greater.")
+// }
+
+
+no1>no2 ? console.log("no1 is greater"):
+no1 == no2? console.log("both are same"):
+console.log("no2 is greater")
+
+var pers = 82;
+
+// if(pers >=80){
+//     console.log("A grade")
+// }
+// else if(pers>=70){
+//     console.log("A grade")
+// }
+// else if (pers>=60){
+//     console.log("C grade")
+// }
+// else{
+//     console.log("fail..")
+// }
+
+pers>=80?console.log("A grade "):
+pers>=70?console.log("B grade "):
+pers>=60?console.log("B grade "):
+console.log("fail..")
