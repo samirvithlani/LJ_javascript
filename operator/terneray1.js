b20ba61ac7224fd00d253e44c48c335967063550
samirvithlani <samir.vithlani83955@gmail.com>
2024-12-30 16:18:18 +0530

    ternery operator

---
+++ operator/terneray1.js
@@ -0,0 +1,45 @@
+//?:
+
+var age =18;
+
+// if(age>=18){
+//     console.log("valid...")
+// }
+// else{
+//     console.log("not valid..")
+// }
+
+age>=18?console.log("valid"):console.log("not valid..")
+
+
+var no1=100
+var no2 =200
+
+no1>no2?console.log("no1 is grrter"):console.log("no2 is greater.")
+// var max;
+
+// if(no1>no2){
+
+//     max = no1;
+// }
+// else{
+//     max = no2;
+// }
+var max = no1>no2?no1:no2
+console.log("max ",max)
+
+
+
+var name = "raj"
+// var flag;
+// if(name.startsWith("r")){
+//     flag = true;
+// }
+// else{
+//     flag = false;
+// }
+// console.log("flag =",flag)
+
+var flag = name.startsWith("m")?true:false
+console.log(flag)
+
