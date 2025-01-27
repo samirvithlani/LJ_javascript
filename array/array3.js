76e2dcaa1f56d5cc77b42df8dec842449e6016ee
samirvithlani <samir.vithlani83955@gmail.com>
2025-01-27 16:57:24 +0530

    function

---
+++ array/array3.js
@@ -0,0 +1,8 @@
+var users = ["amit","sumit","shyam","seeta","shyam","geeta"]
+
+var isAvailable = users.includes("Sumit")
+console.log(isAvailable)
+
+//var ind = users.indexOf("shyam",3)
+//var ind = users.lastIndexOf("shyam",3)
+console.log("index",ind)
