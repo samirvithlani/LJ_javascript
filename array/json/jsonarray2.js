b881bdede9625a226d8d9c414a38efc5e4dcc789
samirvithlani <samir.vithlani83955@gmail.com>
2025-02-10 16:20:06 +0530

    map

---
+++ array/json/jsonarray2.js
@@ -0,0 +1,14 @@
+var data = [
+    {
+        id:101,
+        name:"India",
+        capital:"Delhi",
+        pm:"Modi",
+        populatoin:"1.4b"
+    },
+    //....
+]
+
+
+//[{country:"india",pm:"modi"}]
+//[{country:"india",capital"delhi"},{}]
